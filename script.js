6a082237bbb036ab561743342d3323ca7dc3d9e3
I-DMK <ivan.damasek@gmail.com>
2025-03-15 09:00:39 +0100

    Added getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice()); // Run multiple times to see random choices
+
